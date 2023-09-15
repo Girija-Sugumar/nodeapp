7921df0dfc337a483ac6a61f6dcd2bbd90396271
Girija-Sugumar <133954729+Girija-Sugumar@users.noreply.github.com>
2023-09-15 14:56:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Jenkins Kubernetes Deploy')
+  res.send('Jenkins Kubernetes Deployment')
 })
 
 app.get('/app', (req, res) => {
