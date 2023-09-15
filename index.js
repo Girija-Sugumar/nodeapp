b82a3f0c9a2525d9d220f44d1674246faf7c1265
Girija-Sukumar <girija.sukumar@softsuave.org>
2023-09-15 10:32:47 +0530

    updated index.js and kube.yaml file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('AKS practicing')
+  res.send('Jenkins Kubernetes Deploy')
 })
 
-app.get('/lazy', (req, res) => {
-  res.send('blue-green deployment')
+app.get('/app', (req, res) => {
+  res.send('Jenkins Pipeline with kubernetes configuration')
 })
 
 app.listen(port, () => {
