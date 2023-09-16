c00e79d8ecf04e5fd62f9d1b4a43eb10ad99cb6f
Girija-Sukumar <girija.sukumar@softsuave.org>
2023-09-16 12:34:57 +0530

    last push

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
   res.send('Jenkins Kubernetes Deployments')
-=======
-  res.send('Jenkins Kubernetes Deploymentss')
->>>>>>> 3d94ba3aae61700fd11378639402a665af18d265
 })
 
 app.get('/app', (req, res) => {
